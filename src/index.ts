848b35f49b6586d9ebf4faa4bcdd91634d79afba
Robert Kardos <kardos.robert.kh@gmail.com>
2017-04-06 19:08:09 +0200

    Add splash screen and menu

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,47 @@ let planet = new PIXI.extras.TilingSprite(planetTexture, 800, 252);
 planet.y = 600 - 252;
 backgroundContainer.addChild(planet);
 
+let menuTexture: PIXI.Texture = PIXI.Texture.fromImage('assets/menu.png');
+let menu = new PIXI.extras.TilingSprite(menuTexture, 800, 600);
+backgroundContainer.addChild(menu);
 
-app.ticker.add(function(delta) {
+let splashscreenTexture = PIXI.Texture.fromImage('assets/splashscreen.png');
+let splashscreen = new PIXI.Sprite(splashscreenTexture);
+let blurFilter = new PIXI.filters.BlurFilter();
+blurFilter.blur = 0;
+
+splashscreen.filters = [blurFilter];
+
+backgroundContainer.addChild(splashscreen);
+
+let runningSince = 0;
+
+// let ticker = PIXI.ticker.shared;
+let splashscreenTicker = new PIXI.ticker.Ticker();
+splashscreenTicker.autoStart = true;
+
+splashscreenTicker.add(function(delta) {
+	if (runningSince + splashscreenTicker.elapsedMS > 2000) {
+		splashscreen.alpha -= 0.01;
+		blurFilter.blur += 0.1;
+
+		if (!menuTicker.started) {
+			menuTicker.start();
+		}
+		if (splashscreen.alpha <= 0) {
+			splashscreenTicker.stop();
+		}
+	}
+
+	runningSince += menuTicker.elapsedMS;
+
+});
+
+splashscreenTicker.start();
+
+let menuTicker = new PIXI.ticker.Ticker();
+menuTicker.add(function(delta) {
+	menu.tilePosition.x -= 8;
 	planet.tilePosition.x -= 3;
 	space.tilePosition.x -= 0.05;
 });
